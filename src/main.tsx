59a4a4d5bc78e08eb98592100ee31489f4992d77
Rais Alam <rais@cloudeagle.ai>
2026-03-26 12:47:33 +0530

    feat: init remote config and log values

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@
   import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { initRemoteConfig, getRemoteAll } from './services';
+
+initRemoteConfig()
+  .then(() => {
+    const all = getRemoteAll();
+    console.log('[RemoteConfig] keys:', Object.keys(all));
+    Object.entries(all).forEach(([key, value]) => {
+      console.log(`[RemoteConfig] ${key} =`, value.asString());
+    });
+  })
+  .catch((err) => {
+    console.error('[RemoteConfig] init failed', err);
+  });
   import "./styles/index.css";
 
   createRoot(document.getElementById("root")!).render(
